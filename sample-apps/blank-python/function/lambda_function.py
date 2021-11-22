5d1c15eada93c1653951c18d97e93e35e715d47f
FitzsimmonsGiuseppe <fitzsimmonsgiuseppe@gmail.com>
2021-11-22 19:48:19 +0100

    python stuff

--- sample-apps/blank-python/function/lambda_function.py
+++ sample-apps/blank-python/function/lambda_function.py
@@ -1,20 +1,27 @@
 import os
 import logging
-import jsonpickle
-import boto3
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
+#import jsonpickle
+#import boto3
+import geoip2.webservice
+import sys
+import json
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-patch_all()
+redirect = json.loads(open('./redirect.json').read().encode('ascii'))
 
-client = boto3.client('lambda')
-client.get_account_settings()
+#codicil isn't the only campaign, get a url parser and find out which one we're on and use that as the first index of redirect
 
 def lambda_handler(event, context):
-    logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
-    logger.info('## EVENT\r' + jsonpickle.encode(event))
-    logger.info('## CONTEXT\r' + jsonpickle.encode(context))
-    response = client.get_account_settings()
-    return response['AccountUsage']
+    #move this line to an outside test script, so you don't pollute this with test-specific shit
+    if(type(event) is str):
+        event = (json.loads(open(sys.argv[1], 'r').read()))
+    response = geoip2.webservice.Client(639089, 'pKA4mu9VIQuHs7E9', host='geolite.info').country(event['requestContext']['sourceIp'])
+    try:
+        url_code = redirect['codicil'][response.country.iso_code]
+        print('https://indie-fensible.pub/'+str(url_code)+'/googleads')
+        return 'https://indie-fensible.pub/'+str(url_code)+'/googleads'
+    except KeyError:
+        url_code = redirect['codicil']['default']
+        print('https://indie-fensible.pub/'+str(url_code)+'/googleads')
+        return 'https://indie-fensible.pub/'+str(url_code)+'/googleads'
+
+lambda_handler(sys.argv[1], sys.argv[2])
