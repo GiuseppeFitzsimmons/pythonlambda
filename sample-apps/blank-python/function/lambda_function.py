96e42a231d99e2f60da902e05ec3d7e8dba536f6
FitzsimmonsGiuseppe <fitzsimmonsgiuseppe@gmail.com>
2021-11-24 15:09:59 +0100

    extra error handling, real tests are set up

--- sample-apps/blank-python/function/lambda_function.py
+++ sample-apps/blank-python/function/lambda_function.py
@@ -1,9 +1,4 @@
-import os
-import logging
-#import jsonpickle
-#import boto3
 import geoip2.webservice
-import sys
 import json
 from datetime import date
 
@@ -11,10 +6,7 @@ redirect = json.loads(open('./redirect.json').read().encode('ascii'))
 error_object={'error':'error, probably failed to parse path'}
 
 def lambda_handler(event, context):
-    #move this line to an outside test script, so you don't pollute this with test-specific shit
     try:
-        if(type(event) is str):
-            event = (json.loads(open(sys.argv[1], 'r').read()))
         response = {}
         event_path = str(event['requestContext']['path'])
         book_campaign = str(event_path).split('/')[3]
@@ -23,12 +15,13 @@ def lambda_handler(event, context):
         print('error, probably failed to parse url')
         return (
                     {
-        "statusCode": 502,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": error_object
-    }
+            "statusCode": 307,
+            "headers": {
+                "Content-Type": "application/json",
+                "location":str(redirect['default']['default'])
+            },
+            "body": ''
+        }
         )
     try:
         response = geoip2.webservice.Client(639089, 'pKA4mu9VIQuHs7E9', host='geolite.info').country(event['requestContext']['sourceIp'])
@@ -36,8 +29,11 @@ def lambda_handler(event, context):
         print(json_log)
         try:
             redirect_url = redirect[book_campaign][response.country.iso_code]
-        except KeyError:
-            redirect_url = redirect[book_campaign]['default']
+        except:
+            try:
+                redirect_url = redirect[book_campaign]['default']
+            except:
+                redirect_url = redirect['default']['default']
     except:
         print('Cannot parse IP, setting to default')
         json_log = {"country":"invalid", "platform":platform, "book_campaign": book_campaign, "date":str(date.today()) }
@@ -51,5 +47,3 @@ def lambda_handler(event, context):
             },
             "body": ''
         })
-
-#lambda_handler(sys.argv[1], sys.argv[2])
